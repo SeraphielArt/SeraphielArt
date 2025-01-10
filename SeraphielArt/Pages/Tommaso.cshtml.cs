80de73db5ae73bdfd440c1fa161b3c95a30185ba
Robin Singh <r.singh@gds.com>
2025-01-10 13:29:39 +0100

    Tommaso easter egg

---
+++ SeraphielArt/Pages/Tommaso.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CompuprintScanServer.Pages
+{
+    public class TommasoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
